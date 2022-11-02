1068278baa62e4a0a55c665e939432fceba093cc
Max Reimblad <113595452+MaxReimblad@users.noreply.github.com>
2022-11-02 12:42:00 +0100

    Update Menu.java
    Main Menu

--- YellowPages/src/ApplicationFiles/Menu.java
+++ YellowPages/src/ApplicationFiles/Menu.java
@@ -4,31 +4,36 @@ import java.util.Scanner;
 
 public class Menu {
         Scanner scanner = new Scanner(System.in);
-
+        PrintedSpacing spacing = new PrintedSpacing();
+        MenuMessageOptions messageOptions = new MenuMessageOptions();
+        Profile profile = new Profile();
+        
         boolean quit = false;
+        
         public void runMenu(){
         do {
-            System.out.println("Welcome to the Yellow Pages, user! \nPlease pick one of the options below: \n");
-            System.out.print("""
-                1. Search for profile
-                2. Add a new profile
-                3. Login as admin
-                4. Quit
-                """);
+
+            messageOptions.print();
             String input = scanner.nextLine();
 
             switch (input) {
                 case "1" -> {
                     System.out.println("Searching...");
-
+                    //Code
+                    spacing.print();
                 }
                 case "2" -> {
+                    profile.addDummyProfilesToList();
+                    // Code: addProfile, removeProfile, updateProfile
                     System.out.println("Adding...");
-                    
+                    //profile.printProfileList();
+                    spacing.print();
                 }
                 case "3" -> {
+                    System.out.print("Password: "); String inputPassword = scanner.nextLine();
                     System.out.println("Authenticating...");
-
+                    Authentication.login(inputPassword);
+                    spacing.print();
                 }
                 case "4" -> {
                     System.out.println("Quitting...");
