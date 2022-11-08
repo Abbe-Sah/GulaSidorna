37273fc042c2475b2da6da4a95003ede450edd0f
alkafri <109849556+alkafri@users.noreply.github.com>
2022-11-08 09:29:56 +0100

    Update SearchModule.java

--- YellowPages/src/ApplicationFiles/SearchModule.java
+++ YellowPages/src/ApplicationFiles/SearchModule.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class SearchModule {
 
     Profile profile = new Profile();
-    private ArrayList<ProfileInfo> profileList = new ArrayList<>();
+    private  ArrayList<ProfileInfo> profileList = new ArrayList<>();
 
-    public static void searchMenu (){      // This method list all the options for the search
+    public void searchMenu(){      // This method list all the options for the search
         Scanner scanner = new Scanner(System.in);
         boolean quit = false;
             do{
@@ -22,17 +22,19 @@ public class SearchModule {
 
                 String searchInput = scanner.nextLine();
 
-                if (searchInput == "1"){
-                    System.out.println("anything");
+                if (searchInput.equals("1")){
+                    firstNameSearch (this.profileList);
 
+                } else if (searchInput.equals("2")) {
+                    lastNameSearch (this.profileList);
 
-                } else if (searchInput == "2") {
-                    System.out.println("anything");
-                } else if (searchInput == "3") {
-                    System.out.println("anything");
-                }else if (searchInput == "4") {
-                    System.out.println("anything");
-                }else if (searchInput == "5") {
+                } else if (searchInput.equals("3")) {
+                    addressSearch (this.profileList);
+
+                }else if (searchInput.equals("4")) {
+                    freeSearch (this.profileList);
+
+                }else if (searchInput.equals("5")) {
                     quit = true;
                 } else{
                     System.out.println("Kindly choose one of the above options");
@@ -50,7 +52,6 @@ public class SearchModule {
         for(ProfileInfo profiles : profileList){
             if(profiles.getFirstName().equals(fName)) {
                 System.out.println(profileList.indexOf(profiles) + ": " + profiles.getFirstName() + " " + profiles.getLastName() + " Age: " + profiles.getAge() + ", Phone: " + profiles.getPhoneNumber() + " " + profiles.getAddress());
-                return profile;
             }else {
                 System.out.println("Profile doesn't exist");
                 return null;
@@ -69,11 +70,10 @@ public class SearchModule {
         for(ProfileInfo profiles : profileList){
             if(profiles.getLastName().equals(lName)) {
                 System.out.println(profileList.indexOf(profiles) + ": " + profiles.getFirstName() + " " + profiles.getLastName() + " Age: " + profiles.getAge() + ", Phone: " + profiles.getPhoneNumber() + " " + profiles.getAddress());
-                return profile;
             }else {
                 System.out.println("Profile doesn't exist");
-                return null;
             }
+            return null;
 
         }
         return null;
@@ -88,7 +88,6 @@ public class SearchModule {
         for (ProfileInfo profiles : profileList) {
             if (profiles.getCity().equals(address) || profiles.getStreetName().equals(address)) {
                 System.out.println(profileList.indexOf(profiles) + ": " + profiles.getFirstName() + " " + profiles.getLastName() + " Age: " + profiles.getAge() + ", Phone: " + profiles.getPhoneNumber() + " " + profiles.getAddress());
-                return profile;
             } else {
                 System.out.println("Profile doesn't exist");
                 return null;
@@ -107,7 +106,6 @@ public class SearchModule {
         for (ProfileInfo profiles : profileList) {
             if (profiles.getFirstName().equals(freeSearch) || profiles.getLastName().equals(freeSearch) || profiles.getCity().equals(freeSearch) || profiles.getStreetName().equals(freeSearch)) {
                 System.out.println(profileList.indexOf(profiles) + ": " + profiles.getFirstName() + " " + profiles.getLastName() + " Age: " + profiles.getAge() + ", Phone: " + profiles.getPhoneNumber() + " " + profiles.getAddress());
-                return profile;
             } else {
                 System.out.println("Profile doesn't exist");
                 return null;
