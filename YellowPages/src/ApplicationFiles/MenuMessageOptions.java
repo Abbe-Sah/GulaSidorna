fdaa0d4ce97d394cb67927e33a6aafb8d9b1614a
Max Reimblad <113595452+MaxReimblad@users.noreply.github.com>
2022-11-08 09:51:59 +0100

    Update MenuMessageOptions.java

--- YellowPages/src/ApplicationFiles/MenuMessageOptions.java
+++ YellowPages/src/ApplicationFiles/MenuMessageOptions.java
@@ -4,13 +4,15 @@ public class MenuMessageOptions implements ApplicationMessages {
     @Override
     public void print() {
         System.out.println("""
-                Welcome to the Yellow Pages, user!
-                Please pick one of the options below:""");
+                -------------------------------------
+                Welcome to the Yellow Pages,""" + " " + Authentication.userRole + "!" + """
+                \nPlease pick one of the options below:""");
         System.out.print("""
                 1. Search for profile
                 2. Add a new profile
                 3. Login as admin
                 4. Quit
+                -------------------------------------
                 """);
     }
 }
